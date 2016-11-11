d07b95711f41052332aa9935721c2854e9ddb68b
Daniel <ddegrassi17@gmail.com>
2016-11-11 14:37:44 +0000

    customzed color and font of Brand

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module Delphi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile addition assets
+    config.assets.precompile += %w( .sv .eot .woff .ttf )
+  
   end
 end

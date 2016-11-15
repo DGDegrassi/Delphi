4e7d50ecab6aaca13f4728514502e99201a84d97
Daniel <ddegrassi17@gmail.com>
2016-11-15 13:13:30 +0000

    built a form to save new places using Simple_form

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sass-rails'
 gem 'rails_12factor', group: :production
 gem 'will_paginate'
 gem 'will_paginate-bootstrap'
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.9'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 root 'places#index'
+
+#code that gets ruby to create a bunch of new URLs for us
+resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

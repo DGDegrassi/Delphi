9eb0fb957e816c2487e1fe0c9619db4134a0b173
Daniel <ddegrassi17@gmail.com>
2016-11-16 18:01:03 +0000

    updated destroy place feature to only work for the user that created it.  did not do that last update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 10)
   end
@@ -25,20 +25,23 @@ class PlacesController < ApplicationController
     end
   end
 
-def update
-  @place = Place.find(params[:id])
-  if @place.user != current_user
-      return render text: 'Not Allowed', status: :forbidden
+  def update
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
     end
-  @place.update_attributes(place_params)
-  redirect_to root_path
-end
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
-def destroy
-  @place = Place.find(params[:id])
-  @place.destroy
-  redirect_to root_path
-end
+  def destroy
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+    end
+    @place.destroy
+    redirect_to root_path
+  end
 
   private
 

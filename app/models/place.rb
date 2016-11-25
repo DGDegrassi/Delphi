66042d61b04b3f789be821994aabf1cc93da86af
Daniel <ddegrassi17@gmail.com>
2016-11-25 14:46:51 +0000

    setup database and models for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   validates :name, :address, :description, presence: true
   validates :description, length: { minimum: 3}
   geocoded_by :address

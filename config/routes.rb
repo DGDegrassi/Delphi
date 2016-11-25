8708441b22a0ee2300e3e18ccda49c22a830f91d
Daniel <ddegrassi17@gmail.com>
2016-11-25 15:52:36 +0000

    made the submit button (for adding comments) save the comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 root 'places#index'
 
 #code that gets ruby to create a bunch of new URLs for us
-resources :places
+resources :places do
+  resources :comments, only: :create
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

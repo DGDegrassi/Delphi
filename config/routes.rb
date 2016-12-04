8f0a9d25bad8e3c3e3a85a1ec68668f536602b53
Daniel <ddegrassi17@gmail.com>
2016-12-04 15:51:49 +0000

    created and linked to user page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ root 'places#index'
 resources :places do
   resources :comments, :photos, only: :create
 end
+resources :users, only: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
